10dcd67ccc18d6df9b97deafb4c9b0bdf4caa597
Miklos Sarosi <miklos.sarosi@gmail.com>
2019-05-28 00:21:32 +0000

    first version of moving fields done with one fix needed

--- assets/js/my.js
+++ assets/js/my.js
@@ -2,49 +2,37 @@
 
 var gameInPlay = false;
 var potentialMove = true;
-var gameFields = [2, 4, 4, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 0, 0, 0, 0];
-
-console.log("gameFields array:", gameFields);
-
-formatGameFields();
-
-isThereAMove();
-console.log("before: ", gameFields);
-generateNewField();
-console.log("after: ", gameFields);
-
-
-$("#btn-new-game").click(function() {
-    //the game board reset should come here, now it is for testing functions
-gameInPlay = true;
-gameFields = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-generateNewField();
-generateNewField();
-formatGameFields();
-});
-
-
-
-//checking if there is a potential move, if yes, wait for swipe/button press; if not, game over or undo
-if (potentialMove == false) {
-    alert("Game Over"); // probably a modal is nicer ...
-    gameInPlay = false;
-}
-
+var gameFields = [2, 4, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 0, 0, 0, 0];
+var fieldsForShift = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
+];
+var wasThereAMove = false;
 
 //****************************************************
 // F U N C T I O N S     D E F I N E D       H E R E
 //****************************************************
 
+//Set tile style according to value
+
 function formatGameFields() {
     for (var k = 0; k < 16; k++) {
-        var selectedFieldId = "#gf-" + (k+1);
+        var selectedFieldId = "#gf-" + (k + 1);
         var itsClass = "gf-value-style-" + gameFields[k];
-            for (var m = 0; m<17; m++) {$(selectedFieldId).removeClass("gf-value-style-0").removeClass("gf-value-style-" + Math.pow(2,m));}
-            $(selectedFieldId).addClass(itsClass).text(gameFields[k]);
+        for (var m = 0; m < 18; m++) {
+            $(selectedFieldId)
+                .removeClass("gf-value-style-0")
+                .removeClass("gf-value-style-" + Math.pow(2, m));
+        }
+        $(selectedFieldId)
+            .addClass(itsClass)
+            .text(gameFields[k]);
     }
 }
 
+//check if there is a potential move
 function isThereAMove() {
     potentialMove = false;
     for (var i = 0; i < 15; i++) {
@@ -56,14 +44,156 @@ function isThereAMove() {
             potentialMove = true;
             return potentialMove;
         }
-        console.log("which item: ", i, "th", " value: ", gameFields[i], potentialMove);
     }
 }
 
+// create an array of the fields of the board depending on the direction of the intended move 
+function renderArray(orientation) { //orientation = direction: 0-up, 1-left, 2-down, 3-right
+    var i;
+    var j;
+    if (orientation == 0) { // 0-up
+        for (var k = 0; k < 16; k++) {
+            i = k % 4;
+            j = Math.floor(k / 4);
+            fieldsForShift[i][j] = gameFields[k];
+        }
+    }
+    else if (orientation == 1) { // 1-left
+        for (var k = 0; k < 16; k++) {
+            i = Math.floor(k / 4);
+            j = k % 4;
+            fieldsForShift[i][j] = gameFields[k];
+        }
+    }
+    else if (orientation == 2) { // 2-down
+        for (var k = 0; k < 16; k++) {
+            i = (k % 4);
+            j = 3 - Math.floor(k / 4);
+            fieldsForShift[i][j] = gameFields[k];
+        }
+    }
+    else if (orientation == 3) { //3-right
+        for (var k = 0; k < 16; k++) {
+            i = Math.floor(k / 4);
+            j = 3 - (k % 4);
+            fieldsForShift[i][j] = gameFields[k];
+        }
+    }
+    else {
+        alert("This is impossible... --> there are only four directions :( :( ");
+    }
+}
+
+// re-write the board after the move 
+function reWriteToGameFields(orientation) {
+    var i;
+    var j;
+    if (orientation == 0) { // 0-up
+        for (var k = 0; k < 16; k++) {
+            i = k % 4;
+            j = Math.floor(k / 4);
+            gameFields[k] = fieldsForShift[i][j];
+        }
+        //        for (var ii = 0; ii < 4; ii++) {
+        //          fieldsForShift[ii][4] = 0;
+        //        }
+    }
+
+
+    if (orientation == 1) { // 1-left
+        for (var k = 0; k < 16; k++) {
+            i = Math.floor(k / 4);
+            j = k % 4;
+            gameFields[k] = fieldsForShift[i][j];
+        }
+    }
+    if (orientation == 2) { // 2-down
+        for (var k = 0; k < 16; k++) {
+            i = (k % 4);
+            j = 3 - Math.floor(k / 4);
+            gameFields[k] = fieldsForShift[i][j];
+        }
+    }
+    if (orientation == 3) {
+        for (var k = 0; k < 16; k++) {
+            i = Math.floor(k / 4);
+            j = 3 - (k % 4);
+            gameFields[k] = fieldsForShift[i][j];
+        }
+    }
+}
+
+// the actual moving 
+function shiftFields(direction) { //directions: 0-up, 1-left, 2-down, 3-right
+    renderArray(direction);
+    wasThereAMove = false;
+    for (var i = 0; i < 4; i++) { //For each 'row' do the following
+
+        // Check if there are same value tiles, next to each other, or empty in between them; if yes, add them up and empty shift tiles *******************************************************************
+        if ((fieldsForShift[i][0] != 0) && (fieldsForShift[i][0] == fieldsForShift[i][1])) {
+            fieldsForShift[i][0] = fieldsForShift[i][0] + fieldsForShift[i][1];
+            fieldsForShift[i][1] = 0;
+            wasThereAMove = true;
+
+            if ((fieldsForShift[i][2] != 0) && (fieldsForShift[i][2] == fieldsForShift[i][3])) {
+                fieldsForShift[i][1] = fieldsForShift[i][2] + fieldsForShift[i][3];
+                fieldsForShift[i][2] = 0;
+                fieldsForShift[i][3] = 0;
+                wasThereAMove = true;
+            }
+        }
+        else if ((fieldsForShift[i][1] != 0) && (fieldsForShift[i][1] == fieldsForShift[i][2])) {
+            fieldsForShift[i][1] = fieldsForShift[i][1] + fieldsForShift[i][2];
+            fieldsForShift[i][2] = 0;
+            wasThereAMove = true;
+        }
+        else if ((fieldsForShift[i][2] != 0) && (fieldsForShift[i][2] == fieldsForShift[i][3])) {
+            fieldsForShift[i][2] = fieldsForShift[i][2] + fieldsForShift[i][3];
+            fieldsForShift[i][3] = 0;
+            wasThereAMove = true;
+        }
+
+        // Check if there are empty tiles, if yes, shift all the tiles 
+        for (var x = 0; x < 3; x++) {
+            if (fieldsForShift[i][0] == 0) {
+                if ((fieldsForShift[i][1] + fieldsForShift[i][2] + fieldsForShift[i][3]) != 0) {
+                    fieldsForShift[i][0] = fieldsForShift[i][1];
+                    fieldsForShift[i][1] = fieldsForShift[i][2];
+                    fieldsForShift[i][2] = fieldsForShift[i][3];
+                    fieldsForShift[i][3] = fieldsForShift[i][4];
+                    wasThereAMove = true;
+                }
+            }
+            else { break; }
+        }
+        for (x = 0; x < 2; x++) {
+            if (fieldsForShift[i][1] == 0) {
+                if ((fieldsForShift[i][2] + fieldsForShift[i][3]) != 0) {
+                    fieldsForShift[i][1] = fieldsForShift[i][2];
+                    fieldsForShift[i][2] = fieldsForShift[i][3];
+                    fieldsForShift[i][3] = fieldsForShift[i][4];
+                    wasThereAMove = true;
+                }
+            }
+            else { break; }
+        }
+        for (x = 0; x < 1; x++) {
+            if (fieldsForShift[i][2] == 0) {
+                if (fieldsForShift[i][3] != 0) {
+                    fieldsForShift[i][2] = fieldsForShift[i][3];
+                    fieldsForShift[i][3] = fieldsForShift[i][4];
+                    wasThereAMove = true;
+                }
+            }
+            else { break; }
+        }
+    }
+    reWriteToGameFields(direction);
+    formatGameFields();
+}
 
 //Generate new value for one of the empty fields after a move:
 //Randomise 2 or 4 for on empty field
-
 function generateNewField() {
     var emptyFields = [];
     var n = 0; //counter for number of empty fields
@@ -76,4 +206,89 @@ function generateNewField() {
     var pickPosition = Math.floor(Math.random() * n); //random number between 0 and one less than the number of empty cells 
     var newValue = ((Math.floor(Math.random() * 1.25)) + 1) * 2; // randomising 2 or 4 as a new value, with 80% probability of the 2
     gameFields[emptyFields[pickPosition]] = newValue;
+    formatGameFields();
 }
+
+//****************************************************
+//    E V E N T    H A N D L E R S   
+
+$("#btn-new-game").click(function() {
+    gameInPlay = true;
+    gameFields = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    generateNewField();
+    generateNewField();
+    formatGameFields();
+});
+
+$("#btn-undo").click(function() {
+
+
+});
+
+$("#btn-up").click(function() {
+    if (gameInPlay) {
+        //        debugger;
+        shiftFields(0); //directions: 0-up, 1-left, 2-down, 3-right
+        if (wasThereAMove == true) {
+            generateNewField();
+        }
+        isThereAMove();
+        if (potentialMove == false) {
+            gameInPlay = false;
+            alert("Game Over"); // probably a modal is nicer ...
+        }
+    }
+
+});
+
+$("#btn-left").click(function() {
+    if (gameInPlay) {
+        //        debugger;
+        shiftFields(1); //directions: 0-up, 1-left, 2-down, 3-right
+        if (wasThereAMove == true) {
+            generateNewField();
+        }
+        isThereAMove();
+        if (potentialMove == false) {
+            gameInPlay = false;
+            alert("Game Over"); // probably a modal is nicer ...
+        }
+    }
+
+});
+
+$("#btn-down").click(function() {
+    if (gameInPlay) {
+        //  debugger;
+        shiftFields(2); //directions: 0-up, 1-left, 2-down, 3-right
+        if (wasThereAMove == true) {
+            generateNewField();
+        }
+        isThereAMove();
+        if (potentialMove == false) {
+            gameInPlay = false;
+            alert("Game Over"); // probably a modal is nicer ...
+        }
+    }
+});
+
+$("#btn-right").click(function() {
+    if (gameInPlay) {
+        //  debugger;
+        shiftFields(3); //directions: 0-up, 1-left, 2-down, 3-right
+        if (wasThereAMove == true) {
+            generateNewField();
+        }
+        isThereAMove();
+        if (potentialMove == false) {
+            gameInPlay = false;
+            alert("Game Over"); // probably a modal is nicer ...
+        }
+    }
+});
+
+
+//****************************************************
+//    E X E C U T E
+
+formatGameFields();
