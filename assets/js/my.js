6337b93790aa7805cf5b1e0a9a6391d2c708b716
Ubuntu <ubuntu@ip-172-31-84-143.ec2.internal>
2019-07-12 13:03:51 +0000

    experimenting swipe, commit for deploying for testig on mobile

--- assets/js/my.js
+++ assets/js/my.js
@@ -492,6 +492,54 @@ function onUserInput(dir) {
     }
 }
 
+function swipeDetect(theElement, callback){
+  
+    var touchSurface = theElement,
+    swipeDir,
+    startX,
+    startY,
+    distX,
+    distY,
+    threshold = 150, //required min distance traveled to be considered swipe
+    restraint = 100, // maximum distance allowed at the same time in perpendicular direction
+    allowedTime = 300, // maximum time allowed to travel that distance
+    elapsedTime,
+    startTime,
+    handleSwipe = callback || function(swipedir){};
+  
+    touchSurface.addEventListener("touchstart", function(e){
+        var touchobj = e.changedTouches[0];
+        swipeDir = 'none';
+        distX = 0;
+        distY = 0;
+        startX = touchobj.pageX;
+        startY = touchobj.pageY;
+        startTime = new Date().getTime(); // record time when finger first makes contact with surface
+        e.preventDefault();
+    }, false);
+  
+    touchSurface.addEventListener('touchmove', function(e){
+        e.preventDefault(); // prevent scrolling when inside DIV
+    }, false);
+  
+    touchSurface.addEventListener('touchend', function(e){
+        var touchobj = e.changedTouches[0];
+        distX = touchobj.pageX - startX; // get horizontal dist traveled by finger while in contact with surface
+        distY = touchobj.pageY - startY; // get vertical dist traveled by finger while in contact with surface
+        elapsedTime = new Date().getTime() - startTime; // get time elapsed
+        if (elapsedTime <= allowedTime){ // first condition for a swipe met
+            if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ // 2nd condition for horizontal swipe met
+                swipeDir = (distX < 0)? "left" : "right"; // if dist traveled is negative, it indicates left swipe
+            }
+            else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
+                swipeDir = (distY < 0)? "up" : "down"; // if dist traveled is negative, it indicates up swipe
+            }
+        }
+        handleSwipe(swipeDir);
+        e.preventDefault();
+    }, false);
+}
+
 //****************************************************
 //    E V E N T    H A N D L E R S   
 //****************************************************
@@ -568,7 +616,7 @@ window.addEventListener('load', function() {
     var el = document.getElementByClassName("game-board-container");
 
     var hidetimer = null;
-    swipedetect(el, function(swipedir) {
+    swipeDetect(el, function(swipedir) {
         if (swipedir != 'none') {
             var arrowID = "#btn-" + swipedir;
             clearTimeout(hidetimer);
@@ -620,7 +668,7 @@ window.addEventListener('load', function() {
             e.preventDefault()
         }, false)
     */
-}, false) // end window.onload
+}, false); // end window.onload
 
 
 
