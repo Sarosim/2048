0ce206c8a5b0f870726f6484e19bf84b4b111773
Ubuntu <ubuntu@ip-172-31-84-143.ec2.internal>
2019-07-12 12:32:55 +0000

    experimenting swipe deploy for testig on mobile

--- assets/js/my.js
+++ assets/js/my.js
@@ -562,7 +562,67 @@ document.onkeydown = function(e) {
     }
 };
 
-// Touchscreen -- here comes touch screen swipe navigation
+// Touchscreen -- touch screen swipe navigation
+window.addEventListener('load', function() {
+
+    var el = document.getElementByClassName("game-board-container");
+
+    var hidetimer = null;
+    swipedetect(el, function(swipedir) {
+        if (swipedir != 'none') {
+            var arrowID = "#btn-" + swipedir;
+            clearTimeout(hidetimer);
+            $(arrowID).focus();
+            hidetimer = setTimeout(function() { // remove focus from corresponding arrow after 1 second
+               $(arrowID).blur();
+            }, 1000);
+        }
+    });
+
+    /*    var touchsurface = document.getElementById('touchsurface'),
+            startX,
+            startY,
+            dist,
+            threshold = 150, //required min distance traveled to be considered swipe
+            allowedTime = 200, // maximum time allowed to travel that distance
+            elapsedTime,
+            startTime
+     
+        function handleswipe(isrightswipe){
+            if (isrightswipe)
+                touchsurface.innerHTML = 'Congrats, you\'ve made a <span style="color:red">right swipe!</span>'
+            else{
+                touchsurface.innerHTML = 'Condition for right swipe not met yet'
+            }
+        }
+     
+        touchsurface.addEventListener('touchstart', function(e){
+            touchsurface.innerHTML = ''
+            var touchobj = e.changedTouches[0] // reference first touch point (ie: first finger)
+            dist = 0
+            startX = touchobj.pageX
+            startY = touchobj.pageY
+            startTime = new Date().getTime() // record time when finger first makes contact with surface
+            e.preventDefault()
+        }, false)
+     
+        touchsurface.addEventListener('touchmove', function(e){
+            e.preventDefault() // prevent scrolling when inside DIV
+        }, false)
+     
+        touchsurface.addEventListener('touchend', function(e){
+            var touchobj = e.changedTouches[0]
+            dist = touchobj.pageX - startX // get total dist traveled by finger while in contact with surface
+            elapsedTime = new Date().getTime() - startTime // get time elapsed
+            // check that elapsed time is within specified, horizontal dist traveled >= threshold, and vertical dist traveled <= 100
+            var swiperightBol = (elapsedTime <= allowedTime && dist >= threshold && Math.abs(touchobj.pageY - startY) <= 100)
+            handleswipe(swiperightBol)
+            e.preventDefault()
+        }, false)
+    */
+}, false) // end window.onload
+
+
 
 //****************************************************
 //    E X E C U T E
